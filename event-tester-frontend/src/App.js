d9caa3f2eee3083e6ca48da657b3a9ccb19c6948
Jason Kiraly <jaykiraly@gmail.com>
2018-11-07 14:34:12 -0800

    adjusted fe.

--- event-tester-frontend/src/App.js
+++ event-tester-frontend/src/App.js
@@ -5,9 +5,17 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <button className="btn btn-primary">Call Address Emitter</button>
-        <button className="btn btn-primary">Call Counter Emitter</button>
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12 col-sm-12 col-md-12 text-center">
+            <button className="btn btn-primary">Call Address Emitter</button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-12 col-sm-12 col-md-12 text-center">
+            <button className="btn btn-primary">Call Counter Emitter</button>
+          </div>
+        </div>
       </div>
     );
   }
